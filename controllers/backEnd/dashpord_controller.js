59976c8d982155f84c92b762d44bca6a2b1c7305
unknown <zaza909077777@gmail.com>
2022-08-24 16:30:52 +0200

    2update

--- controllers/backEnd/dashpord_controller.js
+++ controllers/backEnd/dashpord_controller.js
@@ -3,12 +3,29 @@ const { tryError } = require("../../Helper/helper");
 /*--------------- start dashpord page ---------------------*/
 const dashpord_page_controller = async (req, res, next) => {
   try {
-    
+    let doctors = await db.doctors.findAndCountAll({});
+    let pationt = await db.users.findAndCountAll({
+      where: {
+        isDoctor: {
+          [Op.ne]: true,
+        },
+      },
+    });
+    let users = await db.users.findAndCountAll({});
+    let specialist = await db.specialist.findAndCountAll({});
+    let medicin = await db.medicin.findAndCountAll({});
+    let labs = await db.labs.findAndCountAll({});
     res.render("backEnd/dashpord", {
       title: "dashpord",
       URL: req.url,
       admin: req.cookies.Admin,
       notification: req.flash("notification")[0],
+      doctors: doctors.count,
+      specialist: specialist.count,
+      pationt: pationt.count,
+      users: users.count,
+      medicin: medicin.count,
+      labs: labs.count,
     });
   } catch (error) {
     tryError(res);
